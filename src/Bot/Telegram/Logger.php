6ebd115fea31df54467e9652a24277ad6e3c7fc0
Ammar Faizi <ammarfaizi2@gmail.com>
2018-08-27 14:13:50 +0700

    Prepare logger

---
+++ src/Bot/Telegram/Logger.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Bot\Telegram;
+
+/**
+ * @author Ammar Faizi <ammarfaizi2@gmail.com> https://www.facebook.com/ammarfaizi2
+ * @license MIT
+ * @package \Bot\Telegram
+ * @version 4.0
+ */
+final class Logger
+{
+	/**
+	 * 
+	 *
+	 */
+	public function __construct(string $json)
+	{
+		$this->data = new Data($json);
+	}
+}
