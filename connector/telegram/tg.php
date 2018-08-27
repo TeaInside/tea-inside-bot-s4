2e4950d33419af5e4b89aa9a8f819c0183e9458b
Ammar Faizi <ammarfaizi2@gmail.com>
2018-08-27 13:28:23 +0700

    Update tg.php

--- connector/telegram/tg.php
+++ connector/telegram/tg.php
@@ -4,5 +4,5 @@ if (isset($argv[1])) {
 	require __DIR__."/../../bootstrap/init.php";
 	require __DIR__."/../../config/telegram/main.php";
 
-	
+	$bot = new \Bot\Telegram\Bot(urldecode($argv[1]));
 }
