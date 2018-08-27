bb61ac0e4dfacc9b8e04aac6eb98ab2070e3aff8
Ammar Faizi <ammarfaizi2@gmail.com>
2018-08-27 12:42:21 +0700

    Fix php binary

--- connector/telegram/first.php
+++ connector/telegram/first.php
@@ -3,9 +3,9 @@
 $input = urlencode(file_get_contents("php://input"));
 
 shell_exec(
-	"nohup ".PHP_BINARY." ".__DIR__."/logger.php \"".$input."\" >> ".__DIR__."/../../logs/telegram/logger.log 2>&1 &"
+	"nohup /usr/bin/php7.2 ".__DIR__."/logger.php \"".$input."\" >> ".__DIR__."/../../logs/telegram/logger.log 2>&1 &"
 );
 
 shell_exec(
-	"nohup ".PHP_BINARY." ".__DIR__."/tg.php \"".$input."\" >> ".__DIR__."/../../logs/telegram/bg.log 2>&1 &"
+	"nohup /usr/bin/php7.2 ".__DIR__."/tg.php \"".$input."\" >> ".__DIR__."/../../logs/telegram/bg.log 2>&1 &"
 );
