22e5ce154f852b310b1fbad1354cb7d9f08feef7
Ammar Faizi <ammarfaizi2@gmail.com>
2018-08-28 13:15:53 +0700

    Message logger

--- src/Bot/Telegram/Logger/Message.php
+++ src/Bot/Telegram/Logger/Message.php
@@ -41,8 +41,7 @@ class Message implements LoggerInterface
 	 */
 	public function run(): void
 	{
-		if (in_array($this->data["msg_type"], ["text"])) {
-			var_dump($this->data["chat_type"]);
+		if (in_array($this->data["msg_type"], ["text", "photo"])) {			
 			if ($this->data["chat_type"] === "group") {
 				$this->groupLogger();
 			} elseif ($this->data["chat_type"] === "private") {
@@ -56,6 +55,8 @@ class Message implements LoggerInterface
 	 */
 	private function groupLogger(): void
 	{
+		$file_id = null;
+
 		$st = $this->pdo->prepare(
 			"INSERT INTO `group_messages` (`group_id`, `user_id`, `telegram_msg_id`, `reply_to_msg_id`, `created_at`, `updated_at`)
 			VALUES (:group_id, :user_id, :telegram_msg_id, :reply_to_msg_id, :created_at, :updated_at);"
@@ -78,6 +79,10 @@ class Message implements LoggerInterface
 
 		$group_message_id = $this->pdo->lastInsertId();
 
+		if (in_array($this->data["msg_type"], ["photo"])) {
+			$file_id = $this->trackFile();
+		}
+
 		$st = $this->pdo->prepare(
 			"INSERT INTO `group_messages_data` (`group_message_id`, `text`, `file_id`, `type`)
 			VALUES (:group_message_id, :text, :file_id, :type);"
@@ -87,7 +92,7 @@ class Message implements LoggerInterface
 			[
 				":group_message_id" => $group_message_id,
 				":text" => $this->data["text"],
-				":file_id" => null,
+				":file_id" => $file_id,
 				":type" => $this->data["msg_type"]
 			]
 		);
@@ -98,6 +103,8 @@ class Message implements LoggerInterface
 	 */
 	private function privateLogger(): void
 	{
+		$file_id = null;
+
 		$st = $this->pdo->prepare(
 			"INSERT INTO `private_messages` (`user_id`, `telegram_msg_id`, `reply_to_msg_id`, `created_at`, `updated_at`)
 			VALUES (:user_id, :telegram_msg_id, :reply_to_msg_id, :created_at, :updated_at);"
@@ -119,6 +126,10 @@ class Message implements LoggerInterface
 
 		$private_message_id = $this->pdo->lastInsertId();
 
+		if (in_array($this->data["msg_type"], ["photo"])) {
+			$file_id = $this->trackFile();
+		}
+
 		$st = $this->pdo->prepare(
 			"INSERT INTO `private_messages_data` (`private_message_id`, `text`, `file_id`, `type`)
 			VALUES (:private_message_id, :text, :file_id, :type);"
@@ -128,9 +139,90 @@ class Message implements LoggerInterface
 			[
 				":private_message_id" => $private_message_id,
 				":text" => $this->data["text"],
-				":file_id" => null,
+				":file_id" => $file_id,
 				":type" => $this->data["msg_type"]
 			]
 		);
+	}
+
+	/**
+	 * @return int
+	 */
+	private function trackFile(): int
+	{
+		switch ($this->data["msg_type"]) {
+			case 'photo':
+					$p = $this->data['photo'][count($this->data['photo']) - 1];
+					$telegram_file_id = $p['file_id'];
+					$st = $this->pdo->prepare(
+						"SELECT `id` FROM `files` WHERE `telegram_file_id`=:telegram_file_id LIMIT 1;"
+					);
+					$st->execute([":telegram_file_id" => $telegram_file_id]);
+					if ($st = $st->fetch(PDO::FETCH_NUM)) {
+						$this->pdo->prepare(
+							"UPDATE `files` SET `hit_count`=`hit_count`+1, `updated_at`=:updated_at WHERE `telegram_file_id`=:telegram_file_id LIMIT 1;"
+						)->execute(
+							[
+								":updated_at" => $this->data["_now"],
+								":telegram_file_id" => $telegram_file_id
+							]
+						);
+						return $st[0];
+					}
+					unset($p["file_id"]);
+					$a = json_decode($out = Exe::getFile(["file_id" => $telegram_file_id])["out"], true);
+
+					if (isset($a["result"]["file_path"])) {
+						unset($out);
+				        $ch = curl_init("https://api.telegram.org/file/bot".TOKEN."/".$a['result']['file_path']);
+				        curl_setopt_array($ch,
+				        	[
+				        		CURLOPT_RETURNTRANSFER => true,
+				        		CURLOPT_SSL_VERIFYPEER => false,
+				        		CURLOPT_SSL_VERIFYHOST => false
+				        	]
+				        );
+
+				        $binary = curl_exec($ch);
+				        curl_close($ch);
+
+				        is_dir(STORAGE_PATH) or mkdir(STORAGE_PATH);
+				        is_dir(STORAGE_PATH."/files") or mkdir(STORAGE_PATH."/files");
+
+				        $filename = ($sha1 = sha1($binary))."_".($md5 = md5($binary)).".jpg";
+				        $handle = fopen(STORAGE_PATH."/files/".$filename, "w");
+				        flock($handle, LOCK_EX);
+				        fwrite($handle, $binary);
+				        fclose($handle);
+
+				        $this->pdo->prepare(
+				        	"INSERT INTO `files` (`type`, `telegram_file_id`, `md5_checksum`, `sha1_checksum`, `absolute_hash`, `hit_count`, `description`, `created_at`, `updated_at`)
+				        	VALUES (:type, :telegram_file_id, :md5_checksum, :sha1_checksum, :absolute_hash, :hit_count, :description, :created_at, :updated_at);"
+				        )->execute(
+				        	[
+				        		":type" => "photo",
+				        		":telegram_file_id" => $telegram_file_id,
+				        		":md5_checksum"  => $md5,
+				        		":sha1_checksum" => $sha1,
+				        		":absolute_hash" => ($sha1."_".$md5),
+				        		":hit_count" => 1,
+				        		":description" => null,
+				        		":created_at" => $this->date["_now"],
+				        		":updated_at" => null
+				        	]
+				        );
+
+				        return $this->pdo->lastInsertId();
+				    } else {
+				    	print "Could not get the file: ".$out."\n\n";
+				    }
+				break;
+			
+			default:
+				
+				break;
+		}
+
+
 	}
 }
