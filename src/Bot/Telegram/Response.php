a839dfbf9a35a6c5abc041ddf56de00391023113
Ammar Faizi <ammarfaizi2@gmail.com>
2018-08-29 13:17:45 +0700

    Prepare response routes

--- src/Bot/Telegram/Response.php
+++ src/Bot/Telegram/Response.php
@@ -2,6 +2,8 @@
 
 namespace Bot\Telegram;
 
+use Closure;
+
 /**
  * @author Ammar Faizi <ammarfaizi2@gmail.com> https://www.facebook.com/ammarfaizi2
  * @license MIT
@@ -10,10 +12,17 @@ namespace Bot\Telegram;
  */
 final class Response
 {
+	use ResponseRoutes;
+
 	/**
 	 * @var \Bot\Telegtram\Data
 	 */
 	private $data;
+
+	/**
+	 * @var array
+	 */
+	private $routes = [];
 
 	/**
 	 * @param \Bot\Telegtram\Data $data
@@ -28,14 +37,30 @@ final class Response
 	 */
 	public function run(): void
 	{
-		if (preg_match("/\/me/Usi", $this->data["text"])) {
-			Exe::sendMessage(
-				[
-					"text" => "There is no data stored for this user.",
-					"chat_id" => $this->data["chat_id"],
-					"reply_to_message_id" => $this->data["msg_id"]
-				]
-			);
+		$this->buildRoutes();
+		foreach ($this->routes as $key => $route) {
+			$route[0] = $route[0]();
+			if (isset($route[0], $route[1]) && is_array($route[1])) {
+				if ($route[0][0]) {
+					if (is_string($route[1])) {
+						
+					} elseif ($route[1] instanceof Closure) {
+						if ($route[1](...$route[1])) {
+							break;		
+						}
+					}
+				}
+			}
 		}
+	}
+
+	/**
+	 * @param \Closure $cond
+	 * @param mixed    $action
+	 * @return void
+	 */
+	private function set(Closure $cond, $action): void
+	{
+		$this->routes[] = [$cond, $action];
 	}
 }
---
+++ src/Bot/Telegram/ResponseRoutes.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Bot\Telegram;
+
+/**
+ * @author Ammar Faizi <ammarfaizi2@gmail.com> https://www.facebook.com/ammarfaizi2
+ * @license MIT
+ * @package \Bot\Telegram
+ * @version 4.0
+ */
+trait ResponseRoutes
+{
+	/**
+	 * @return void
+	 */
+	private function buildRoutes(): void
+	{
+		if (preg_match("/\/me/Usi", $this->data["text"])) {
+			$this->set(function() {
+				return [true, []];
+			}, function () {
+				Exe::sendMessage(
+					[
+						"text" => "There is no data stored for this user.",
+						"chat_id" => $this->data["chat_id"],
+						"reply_to_message_id" => $this->data["msg_id"]
+					]
+				);
+			});
+		}
+	}
+}
