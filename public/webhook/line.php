bbe013ca3e44ecc8505fd3c644596aa82c80d79e
Ammar Faizi <ammarfaizi2@gmail.com>
2018-09-08 23:30:01 +0700

    Create webhook key and update .gitignore

---
+++ public/webhook/line.php
@@ -0,0 +1,12 @@
+<?php
+
+if (isset($_GET["key"])) {
+	require __DIR__."/../../config/line/webhook_key.php";
+	if ($_GET["key"] !== WEBHOOK_KEY) {
+		http_response_code(403);
+		header("Content-Type: text/plain");
+		exit("Forbidden!");
+	}
+}
+
+require __DIR__."/../../connector/line/first.php";
