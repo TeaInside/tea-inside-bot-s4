3f9248f4c35659af1b6eb9db869d7343a4d39393
Ammar Faizi <ammarfaizi2@gmail.com>
2018-09-14 14:21:49 +0700

    Prepare facebook config

---
+++ config/facebook/hub_verify_token.php
@@ -0,0 +1,3 @@
+<?php
+
+define("HUB_VERIFY_TOKEN", "496ea7bce9ea46ab313ae2945c776ec66acc1942");
--- connector/facebook/first.php
+++ connector/facebook/first.php
@@ -1,5 +1,28 @@
 <?php
 
+header("Content-Type: text/plain");
+
+if (isset($_REQUEST["hub_mode"], $_REQUEST["hub_challenge"], $_REQUEST["hub_verify_token"])) {
+	
+	require __DIR__."/../../config/facebook/hub_verify_token.php";
+	
+	switch ($_REQUEST["hub_mode"]) {
+		case 'subscribe':
+			if (! defined("HUB_VERIFY_TOKEN")) {
+				exit("HUB_VERIFY_TOKEN is not defined yet!\n");
+			}
+			if ($_REQUEST["hub_verify_token"] === HUB_VERIFY_TOKEN) {
+				print $_REQUEST["hub_challenge"];
+			}		
+			break;
+		
+		default:
+			break;
+	}
+	exit;
+}
+
+
 $input = urlencode(file_get_contents("php://input"));
 
 shell_exec(
@@ -12,5 +35,4 @@ shell_exec(
 
 file_put_contents(__DIR__."/../../logs/facebook/first.log", 
 	json_encode(json_decode($input), 128)
-
 );
