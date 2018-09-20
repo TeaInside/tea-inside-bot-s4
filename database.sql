5d598039c2e6ea2faf9a8056b40446b7264b5696
Ammar Faizi <ammarfaizi2@gmail.com>
2018-09-20 12:39:28 +0700

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,98 +1,52 @@
--- Adminer 4.6.3 MySQL dump
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 20, 2018 at 07:37 AM
+-- Server version: 10.1.35-MariaDB
+-- PHP Version: 7.2.9
 
-SET NAMES utf8;
-SET time_zone = '+00:00';
-SET foreign_key_checks = 0;
-SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-SET NAMES utf8mb4;
 
-DROP TABLE IF EXISTS `files`;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `icetea_telegram`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `files`
+--
+
 CREATE TABLE `files` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `id` bigint(20) NOT NULL,
   `type` varchar(32) NOT NULL,
-  `telegram_file_id` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `telegram_file_id` varchar(255) NOT NULL,
   `md5_checksum` varchar(32) NOT NULL,
   `sha1_checksum` varchar(40) NOT NULL,
   `absolute_hash` varchar(73) NOT NULL,
   `hit_count` varchar(73) NOT NULL DEFAULT '0',
   `description` text CHARACTER SET utf8mb4 COLLATE utf8mb4_bin,
   `created_at` datetime NOT NULL,
-  `updated_at` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `absolute_hash` (`absolute_hash`),
-  UNIQUE KEY `file_telegram_id` (`telegram_file_id`),
-  KEY `md5_checksum` (`md5_checksum`),
-  KEY `sha1_checksum` (`sha1_checksum`),
-  FULLTEXT KEY `description` (`description`)
+  `updated_at` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
 
-DROP TABLE IF EXISTS `group_admin`;
-CREATE TABLE `group_admin` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `group_id` varchar(64) NOT NULL,
-  `user_id` varchar(64) NOT NULL,
-  `role` varchar(255) NOT NULL,
-  `created_at` datetime NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `group_id` (`group_id`),
-  KEY `user_id` (`user_id`),
-  CONSTRAINT `group_admin_ibfk_4` FOREIGN KEY (`group_id`) REFERENCES `groups` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `group_admin_ibfk_5` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+--
+-- Table structure for table `groups`
+--
 
-
-DROP TABLE IF EXISTS `group_history`;
-CREATE TABLE `group_history` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `group_id` varchar(64) NOT NULL,
-  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
-  `username` varchar(255) DEFAULT NULL,
-  `link` varchar(255) DEFAULT NULL,
-  `photo` varchar(255) DEFAULT NULL,
-  `created_at` datetime NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `group_id` (`group_id`),
-  KEY `name` (`name`),
-  KEY `username` (`username`),
-  KEY `link` (`link`),
-  KEY `photo` (`photo`),
-  CONSTRAINT `group_history_ibfk_2` FOREIGN KEY (`group_id`) REFERENCES `groups` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-
-DROP TABLE IF EXISTS `group_messages`;
-CREATE TABLE `group_messages` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `group_id` varchar(64) NOT NULL,
-  `user_id` varchar(64) NOT NULL,
-  `telegram_msg_id` bigint(20) NOT NULL,
-  `reply_to_msg_id` bigint(20) DEFAULT NULL,
-  `created_at` datetime NOT NULL,
-  `updated_at` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `group_id` (`group_id`),
-  KEY `user_id` (`user_id`),
-  CONSTRAINT `group_messages_ibfk_3` FOREIGN KEY (`group_id`) REFERENCES `groups` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `group_messages_ibfk_4` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-
-DROP TABLE IF EXISTS `group_messages_data`;
-CREATE TABLE `group_messages_data` (
-  `group_message_id` bigint(20) NOT NULL,
-  `text` text CHARACTER SET utf8mb4 COLLATE utf8mb4_bin,
-  `file_id` bigint(20) DEFAULT NULL,
-  `type` varchar(32) NOT NULL,
-  KEY `group_message_id` (`group_message_id`),
-  KEY `file_id` (`file_id`),
-  CONSTRAINT `group_messages_data_ibfk_3` FOREIGN KEY (`group_message_id`) REFERENCES `group_messages` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `group_messages_data_ibfk_4` FOREIGN KEY (`file_id`) REFERENCES `files` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-
-DROP TABLE IF EXISTS `groups`;
 CREATE TABLE `groups` (
   `id` varchar(64) NOT NULL,
   `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
@@ -101,77 +55,134 @@ CREATE TABLE `groups` (
   `photo` varchar(255) DEFAULT NULL,
   `msg_count` bigint(20) NOT NULL DEFAULT '0',
   `created_at` datetime NOT NULL,
-  `updated_at` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `name` (`name`),
-  KEY `username` (`username`),
-  KEY `link` (`link`),
-  KEY `photo` (`photo`)
+  `updated_at` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
 
-DROP TABLE IF EXISTS `private_messages`;
-CREATE TABLE `private_messages` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+--
+-- Table structure for table `group_admin`
+--
+
+CREATE TABLE `group_admin` (
+  `id` bigint(20) NOT NULL,
+  `group_id` varchar(64) NOT NULL,
+  `user_id` varchar(64) NOT NULL,
+  `role` varchar(255) NOT NULL,
+  `created_at` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `group_history`
+--
+
+CREATE TABLE `group_history` (
+  `id` bigint(20) NOT NULL,
+  `group_id` varchar(64) NOT NULL,
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  `username` varchar(255) DEFAULT NULL,
+  `link` varchar(255) DEFAULT NULL,
+  `photo` varchar(255) DEFAULT NULL,
+  `created_at` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `group_messages`
+--
+
+CREATE TABLE `group_messages` (
+  `id` bigint(20) NOT NULL,
+  `group_id` varchar(64) NOT NULL,
   `user_id` varchar(64) NOT NULL,
   `telegram_msg_id` bigint(20) NOT NULL,
   `reply_to_msg_id` bigint(20) DEFAULT NULL,
+  `telegram_datetime` bigint(20) DEFAULT NULL,
   `created_at` datetime NOT NULL,
-  `updated_at` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `user_id` (`user_id`),
-  CONSTRAINT `private_messages_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+  `updated_at` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
 
-DROP TABLE IF EXISTS `private_messages_data`;
+--
+-- Table structure for table `group_messages_data`
+--
+
+CREATE TABLE `group_messages_data` (
+  `group_message_id` bigint(20) NOT NULL,
+  `text` text CHARACTER SET utf8mb4 COLLATE utf8mb4_bin,
+  `file_id` bigint(20) DEFAULT NULL,
+  `type` varchar(32) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `group_settings`
+--
+
+CREATE TABLE `group_settings` (
+  `group_id` varchar(64) CHARACTER SET latin1 NOT NULL,
+  `silent` tinyint(4) NOT NULL DEFAULT '1',
+  `welcome_message` text,
+  `max_warns` int(11) NOT NULL DEFAULT '3',
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `private_messages`
+--
+
+CREATE TABLE `private_messages` (
+  `id` bigint(20) NOT NULL,
+  `user_id` varchar(64) NOT NULL,
+  `telegram_msg_id` bigint(20) NOT NULL,
+  `reply_to_msg_id` bigint(20) DEFAULT NULL,
+  `telegram_datetime` bigint(20) DEFAULT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `private_messages_data`
+--
+
 CREATE TABLE `private_messages_data` (
   `private_message_id` bigint(20) NOT NULL,
   `text` text CHARACTER SET utf8mb4 COLLATE utf8mb4_bin,
   `file_id` bigint(20) DEFAULT NULL,
-  `type` varchar(32) NOT NULL,
-  KEY `private_message_id` (`private_message_id`),
-  KEY `file_id` (`file_id`),
-  FULLTEXT KEY `text` (`text`),
-  CONSTRAINT `private_messages_data_ibfk_2` FOREIGN KEY (`private_message_id`) REFERENCES `private_messages` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `private_messages_data_ibfk_4` FOREIGN KEY (`file_id`) REFERENCES `files` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
+  `type` varchar(32) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
 
-DROP TABLE IF EXISTS `sudoers`;
+--
+-- Table structure for table `sudoers`
+--
+
 CREATE TABLE `sudoers` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `id` bigint(20) NOT NULL,
   `user_id` varchar(64) NOT NULL,
   `password` varchar(128) DEFAULT NULL,
   `session_timeout` bigint(20) DEFAULT '-1',
   `created_at` datetime NOT NULL,
-  `latest_logged_in` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `user_id` (`user_id`),
-  CONSTRAINT `sudoers_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+  `latest_logged_in` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
 
-DROP TABLE IF EXISTS `user_history`;
-CREATE TABLE `user_history` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `user_id` varchar(64) NOT NULL,
-  `first_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
-  `last_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
-  `username` varchar(255) DEFAULT NULL,
-  `photo` varchar(255) DEFAULT NULL,
-  `created_at` datetime NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `user_id` (`user_id`),
-  KEY `first_name` (`first_name`),
-  KEY `last_name` (`last_name`),
-  KEY `username` (`username`),
-  KEY `photo` (`photo`),
-  CONSTRAINT `user_history_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+--
+-- Table structure for table `users`
+--
 
-
-DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
   `id` varchar(64) NOT NULL,
   `first_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
@@ -182,13 +193,242 @@ CREATE TABLE `users` (
   `private_message_count` bigint(20) NOT NULL DEFAULT '0',
   `group_message_count` bigint(20) NOT NULL DEFAULT '0',
   `created_at` datetime NOT NULL,
-  `updated_at` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `first_name` (`first_name`),
-  KEY `last_name` (`last_name`),
-  KEY `username` (`username`),
-  KEY `photo` (`photo`)
+  `updated_at` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
 
--- 2018-09-08 16:30:55
+--
+-- Table structure for table `user_history`
+--
+
+CREATE TABLE `user_history` (
+  `id` bigint(20) NOT NULL,
+  `user_id` varchar(64) NOT NULL,
+  `first_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  `last_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
+  `username` varchar(255) DEFAULT NULL,
+  `photo` varchar(255) DEFAULT NULL,
+  `created_at` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `files`
+--
+ALTER TABLE `files`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `absolute_hash` (`absolute_hash`),
+  ADD UNIQUE KEY `file_telegram_id` (`telegram_file_id`),
+  ADD KEY `md5_checksum` (`md5_checksum`),
+  ADD KEY `sha1_checksum` (`sha1_checksum`);
+ALTER TABLE `files` ADD FULLTEXT KEY `description` (`description`);
+
+--
+-- Indexes for table `groups`
+--
+ALTER TABLE `groups`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `name` (`name`(191)),
+  ADD KEY `username` (`username`),
+  ADD KEY `link` (`link`),
+  ADD KEY `photo` (`photo`);
+
+--
+-- Indexes for table `group_admin`
+--
+ALTER TABLE `group_admin`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `group_id` (`group_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `group_history`
+--
+ALTER TABLE `group_history`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `group_id` (`group_id`),
+  ADD KEY `name` (`name`(191)),
+  ADD KEY `username` (`username`),
+  ADD KEY `link` (`link`),
+  ADD KEY `photo` (`photo`);
+
+--
+-- Indexes for table `group_messages`
+--
+ALTER TABLE `group_messages`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `group_id` (`group_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `group_messages_data`
+--
+ALTER TABLE `group_messages_data`
+  ADD KEY `group_message_id` (`group_message_id`),
+  ADD KEY `file_id` (`file_id`);
+
+--
+-- Indexes for table `group_settings`
+--
+ALTER TABLE `group_settings`
+  ADD KEY `group_id` (`group_id`);
+
+--
+-- Indexes for table `private_messages`
+--
+ALTER TABLE `private_messages`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `private_messages_data`
+--
+ALTER TABLE `private_messages_data`
+  ADD KEY `private_message_id` (`private_message_id`),
+  ADD KEY `file_id` (`file_id`);
+ALTER TABLE `private_messages_data` ADD FULLTEXT KEY `text` (`text`);
+
+--
+-- Indexes for table `sudoers`
+--
+ALTER TABLE `sudoers`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `first_name` (`first_name`(191)),
+  ADD KEY `last_name` (`last_name`(191)),
+  ADD KEY `username` (`username`),
+  ADD KEY `photo` (`photo`);
+
+--
+-- Indexes for table `user_history`
+--
+ALTER TABLE `user_history`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `first_name` (`first_name`(191)),
+  ADD KEY `last_name` (`last_name`(191)),
+  ADD KEY `username` (`username`),
+  ADD KEY `photo` (`photo`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `files`
+--
+ALTER TABLE `files`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `group_admin`
+--
+ALTER TABLE `group_admin`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `group_history`
+--
+ALTER TABLE `group_history`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `group_messages`
+--
+ALTER TABLE `group_messages`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `private_messages`
+--
+ALTER TABLE `private_messages`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `sudoers`
+--
+ALTER TABLE `sudoers`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `user_history`
+--
+ALTER TABLE `user_history`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `group_admin`
+--
+ALTER TABLE `group_admin`
+  ADD CONSTRAINT `group_admin_ibfk_4` FOREIGN KEY (`group_id`) REFERENCES `groups` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `group_admin_ibfk_5` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `group_history`
+--
+ALTER TABLE `group_history`
+  ADD CONSTRAINT `group_history_ibfk_2` FOREIGN KEY (`group_id`) REFERENCES `groups` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `group_messages`
+--
+ALTER TABLE `group_messages`
+  ADD CONSTRAINT `group_messages_ibfk_3` FOREIGN KEY (`group_id`) REFERENCES `groups` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `group_messages_ibfk_4` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `group_messages_data`
+--
+ALTER TABLE `group_messages_data`
+  ADD CONSTRAINT `group_messages_data_ibfk_3` FOREIGN KEY (`group_message_id`) REFERENCES `group_messages` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `group_messages_data_ibfk_4` FOREIGN KEY (`file_id`) REFERENCES `files` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+--
+-- Constraints for table `group_settings`
+--
+ALTER TABLE `group_settings`
+  ADD CONSTRAINT `group_settings_ibfk_2` FOREIGN KEY (`group_id`) REFERENCES `groups` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `private_messages`
+--
+ALTER TABLE `private_messages`
+  ADD CONSTRAINT `private_messages_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `private_messages_data`
+--
+ALTER TABLE `private_messages_data`
+  ADD CONSTRAINT `private_messages_data_ibfk_2` FOREIGN KEY (`private_message_id`) REFERENCES `private_messages` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `private_messages_data_ibfk_4` FOREIGN KEY (`file_id`) REFERENCES `files` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+--
+-- Constraints for table `sudoers`
+--
+ALTER TABLE `sudoers`
+  ADD CONSTRAINT `sudoers_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `user_history`
+--
+ALTER TABLE `user_history`
+  ADD CONSTRAINT `user_history_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
