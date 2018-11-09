e0acd8691fe9e543057168f5636e14048a8be99c
Ammar Faizi <ammarfaizi2@gmail.com>
2018-11-09 17:29:23 +0700

    Rename some connectors name

--- connector/telegram/webhook.php
+++ connector/telegram/webhook.php
@@ -3,7 +3,7 @@
 $input = escapeshellarg(file_get_contents("php://input"));
 
 shell_exec(
-	"nohup /usr/bin/php7.2 ".__DIR__."/tg.php {$input} >> ".__DIR__."/../../logs/telegram/bg.log 2>&1 &"
+	"nohup /usr/bin/php7.2 ".__DIR__."/bot.php {$input} >> ".__DIR__."/../../logs/telegram/bg.log 2>&1 &"
 );
 
 shell_exec(
