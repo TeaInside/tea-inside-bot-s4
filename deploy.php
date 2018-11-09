8b24f555c11ed2d433d443b10db7ea5479ee792d
Ammar Faizi <ammarfaizi2@gmail.com>
2018-11-09 17:15:44 +0700

    Add circleci config and auto deploy

---
+++ deploy.php
@@ -0,0 +1,14 @@
+<?php
+
+$ch = curl_init("https://webhook.teainside.org/circleci/tea-inside-bot-s4.php");
+curl_setopt_array($ch, 
+	[
+		CURLOPT_POST => true,
+		CURLOPT_POSTFIELDS => "",
+		CURLOPT_RETURNTRANSFER => true,
+		CURLOPT_SSL_VERIFYPEER => false,
+		CURLOPT_SSL_VERIFYHOST => false
+	]
+);
+print curl_exec($ch);
+curl_close($ch);
